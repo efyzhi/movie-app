de23af6720ccabe2b6d1aeece7a3c9a4db2a87d8
efyzhi <pelurobert10@gmail.com>
2023-04-22 08:44:33 +0100

    added gssp

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,12 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
 import styles from '@/components/styles/Home.module.css'
 import SearchMovies from '../src/components/SearchMovies'
-// import styles from '@e/components/styles/global.css'
 
 
 export default function Home() {
   return (
     <>
         <div className={styles.container}>
-          <h1 className={styles.title}> Movie Seearch</h1>
+          <h1 className={styles.title}> Movie Search</h1>
           <SearchMovies />
         </div>
     </>
--- src/components/SearchMovies.jsx
+++ src/components/SearchMovies.jsx
@@ -2,35 +2,31 @@ import React, { useState } from "react";
 import styles from "/styles/SearchMovies.module.css";
 import MovieCard from "./MovieCard";
 
-const SearchMovies = () => {
+const SearchMovies = ({ data }) => {
   const [query, setQuery] = useState("");
-  const [movies, setMovies] = useState([]);
+  const [movies, setMovies] = useState(data);
 
   const handleQuery = (e) => {
     setQuery(e.target.value);
   };
 
-  const SearchMovies = async (e) => {
+  const searchMovies = async (e) => {
     e.preventDefault();
-    console.log("submitting");
 
     const url = `https://api.themoviedb.org/3/search/movie?api_key=5a3cae9931be2c3de8c462e67d6b9331&language=en-US&query=${query}&page=1&include_adult=false`;
 
     try {
       const res = await fetch(url);
       const data = await res.json();
-      console.log(data.results);
       setMovies(data.results);
     } catch (error) {
       console.error(error);
-
-      //const url = `https://api.themoviedb.org/3/movie/550?api_key=5a3cae9931be2c3de8c462e67d6b9331`;
     }
   };
 
   return (
     <div>
-      <form className={styles.form} onSubmit={SearchMovies}>
+      <form className={styles.form} onSubmit={searchMovies}>
         <label htmlFor="query" className={styles.label}>
           Movie Name
         </label>
@@ -47,14 +43,38 @@ const SearchMovies = () => {
         </button>
       </form>
       <div className={styles.cardList}>
-        {movies
-          .filter((movie) => movie.poster_path)
-          .map((movie) => (
-            <MovieCard movie={movie} key={movie.id} />
-          ))}
+        {movies &&
+          movies
+            .filter((movie) => movie?.poster_path)
+            .map((movie) => (
+              <MovieCard movie={movie} key={movie.id} />
+            ))}
       </div>
     </div>
   );
 };
+
+export async function getServerSideProps() {
+  const url = `https://api.themoviedb.org/3/search/movie?api_key=5a3cae9931be2c3de8c462e67d6b9331&language=en-US&query=batman&page=1&include_adult=false`;
+
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+
+    return {
+      props: {
+        data: data.results,
+      },
+    };
+  } catch (error) {
+    console.error(error);
+
+    return {
+      props: {
+        data: [],
+      },
+    };
+  }
+}
 
 export default SearchMovies;
